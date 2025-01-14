cb1202630b19a57af633141799d580dec80a2c20
Reyhan9 <namestaknamestak@gmail.com>
2025-01-14 11:19:50 +0000

    006

---
+++ VariableJava.java
@@ -0,0 +1,60 @@
+public class VariableJava  {
+	public static void main(String[] args) {
+
+	//TipeDataNumber
+	//adalah sebuah nilai 
+
+		//IntegerNumbers
+		//adalah sebuah bilangan bulat
+
+			byte iniByte = 100;
+			short iniShort = 1_000;
+			int iniInt = 1_000_000;
+			long iniLong = 1_000_000_000;
+			//IntegerLiterals
+			int litint = 25;
+			int hexint = 0xA132B;
+			int binint = 0b01010101;
+
+		//FloatingPointNumbers
+		//adalah sebuah nilai pecahan atau desimal
+			
+			float iniFloat = 10.10F;
+			double iniDouble = 10.10;
+
+		//note : Ada batasan value di setiap Data Numbernya
+
+	//TipeDataCharacter
+	//adalah sebuah satu karakter huruf
+	//Di reprentasikan dengan kata kunci Char
+	
+		char huruf = 'D'; 
+
+		//note : Nilai character di tandai dengan ''
+
+	//TipeDataBoolean
+	//adalah 2 buah nilai atau kondisi, yaitu true & false
+	//Di reprentasikan dengan kata kunci Boolean
+
+		boolean benar = true;
+		boolean salah = false;
+
+		//note : Default value untuk boolean adalah false
+		
+	//TipeDataString
+	//adalah sebuah sekumpulan character atau kata
+	//Di reprentasikan dengan kata kunci String
+	
+		String text = "test pembelajaran";
+		
+		//note : Nilai string ditandai dengan ""
+		//	 Devault value untuk string adalah Null
+
+	
+
+		System.out.println("file ini hanya hasil pembelajaran java saya git: Reyhan9");
+		System.out.println("Sumber : Youtube ProgrammerZamanNow");
+
+	}
+
+}
